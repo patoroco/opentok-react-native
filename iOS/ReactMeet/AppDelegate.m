833b5bbeb49030d3bbe1995f097e9ebfa2684d4c
Jorge Maroto <patoroco@gmail.com>
2016-10-16 19:49:56 +0200

    It compiles! :D

--- iOS/ReactMeet/AppDelegate.m
+++ iOS/ReactMeet/AppDelegate.m
@@ -31,8 +31,8 @@
    * on the same Wi-Fi network.
    */
   
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
-//  jsCodeLocation = [NSURL URLWithString:@"http://10.150.1.101:8081/index.ios.bundle"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.129:8081/index.ios.bundle"];
 
   /**
    * OPTION 2
